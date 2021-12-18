96ca3ef42039cf5c2c41a24adcc78e4bdc70ce20
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-12-18 10:55:27 +0700

    create select.sql

---
+++ select.sql
@@ -0,0 +1,17 @@
+SELECT * FROM products;
+
+SELECT id, name, price FROM products;
+SELECT `id`, `name`, `price` FROM products;
+
+-- WHERE CLAUSE (case-insensitive)
+SELECT * FROM products
+WHERE quantity = 100;
+
+SELECT * FROM products
+WHERE price = 15000;
+
+SELECT * FROM products
+WHERE id = 'P0001';
+
+SELECT * FROM products
+WHERE name = 'mie ayam bakso';
