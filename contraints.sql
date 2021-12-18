8f928c7d63bc0d1a8d401b44fbd1735164ecec7f
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-12-18 13:28:21 +0700

    update constraints.sql

--- contraints.sql
+++ contraints.sql
@@ -32,3 +32,13 @@ ALTER TABLE products
 -- TEST
 INSERT INTO products(id, name, category, price, quantity)
 VALUES ('P0025', 'Product 5', 'Lain-Lain', 500, 100);
+
+CREATE TABLE products(
+    id          VARCHAR(10) NOT NULL,
+    name        VARCHAR(100) NOT NULL,
+    description TEXT,
+    price       INT UNSIGNED NOT NULL,
+    quantitiy   INT UNSIGNED NOT NULL DEFAULT 0,
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT price_check CHECK ( price >= 1000)
+) ENGINE = InnoDB;
