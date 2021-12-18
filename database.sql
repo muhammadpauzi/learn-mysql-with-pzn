0d6c03369f687d206a4f85d3fb1e90057bb5b215
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-12-18 08:15:55 +0700

    create database.sql

---
+++ database.sql
@@ -0,0 +1,11 @@
+-- FOR SHOW DATABASES
+SHOW DATABASES;
+
+-- CREATE DATABASE - using lowercase and _
+CREATE DATABASE belajar_mysql;
+
+-- DELETE DATABASE
+DROP DATABASE belajar_mysql;
+
+-- CHOOSE DATABASE
+USE belajar_mysql;
