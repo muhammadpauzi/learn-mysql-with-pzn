eff5f8b44ae2663821e80f7aecc0c89004c2e8a0
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-12-19 15:00:43 +0700

    update restore_database.sh

--- restore_database.sh
+++ restore_database.sh
@@ -1,2 +1,8 @@
 # RESTORE/IMPORT DATABASE
 mysql -u root -p belajar_mysql_import < W:\learn-mysql-with-pzn\belajar_mysql.sql
+
+# OR
+> mysql -u root -p
+> create database belajar_mysql_import_source
+> use belajar_mysql_import_source
+> source W:\learn-mysql-with-pzn\belajar_mysql.sql
